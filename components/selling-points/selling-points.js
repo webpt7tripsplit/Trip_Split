44e28ed5a9fc6cf4a2f0fb32955490ffc7d6773e
Dave Kane <davelkane@gmail.com>
2019-06-27 00:50:19 -0500

    JS for selling points background colors complete, responsiveness tweaks, landing page mobile responsiveness complete.

---
+++ components/selling-points/selling-points.js
@@ -0,0 +1,28 @@
+const sellContainers = document.querySelectorAll('.cell');
+
+const outerContainer = document.querySelector('.selling-points');
+const compStyle = getComputedStyle(outerContainer);
+const outerBgColor = compStyle.backgroundColor;
+
+function setbackgroundColor() {
+
+    function darkenColor(color, amt) {
+        let r = parseInt(color.substring(4, 7), 10);
+        let g = parseInt(color.substring(9, 12), 10);
+        let b = parseInt(color.substring(14, 17), 10);
+
+        r = r - amt;
+        g = g - amt;
+        b = b - amt;
+
+        return `rgb(${r}, ${g}, ${b})`
+    }
+
+    sellContainers.forEach((el, i) => {
+
+        el.style.backgroundColor = darkenColor(outerBgColor, (i+1)*2)
+        console.log(el.style.backgroundColor)
+    })
+}
+
+setbackgroundColor();
