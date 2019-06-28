1b50883c2f31913f3e3790bd6ef3876d80d3087a
Dave Kane <davelkane@gmail.com>
2019-06-28 01:03:01 -0500

    SStill working on landing-animation

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -1,4 +1,29 @@
-let stickyNav = document.querySelector('#menu-bar');
+const stickyNav = document.querySelector('#menu-bar');
+const menuIcon = document.querySelector('i');
 
-console.log(stickyNav);
+stickyNav.style.display = 'none';
+// menuIcon.style.display = 'inline-block';
 
+const landing = document.querySelector('.landing');
+
+// const landingCompStyle = getComputedStyle(landing);
+
+// const landingHeight = landingCompStyle.offsetHeight;
+
+// console.log(landingHeight);
+
+// observer = new IntersectionObserver(entry => {
+//     if (entry.intersectionRatio === 0) {
+//         stickyNav.style.display = 'flex';
+//     }
+// });
+
+// observer.observe(landing);
+
+// landing.onvisibilitychange = () => {stickyNav.style.display = 'flex'};
+
+landing.addEventListener('onscroll', (event) => {
+    if (event.target.scrollHeight - event.target.scrollTop === 0) {
+        stickyNav.style.display = "flex";
+    }
+})
