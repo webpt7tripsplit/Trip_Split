daac830f4042aff35866844566864cd9867e4c02
Dave Kane <davelkane@gmail.com>
2019-06-29 15:58:14 -0500

    Tweaked opening position of stickyNav

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -1,12 +1,18 @@
 const stickyNav = document.querySelector('#menu-bar');
 
+const navCompStyles = getComputedStyle(stickyNav);
+
 const landing = document.querySelector('.landing');
 
 if (document.querySelector('title').innerText.includes('Home')) {
-    stickyNav.style.display = 'none';
+    if (landing.scrollHeight - window.scrollY <= stickyNav.offsetHeight) {
+        stickyNav.style.display = "flex";
+    } else {
+        stickyNav.style.display = 'none';
+    }
 
     window.addEventListener('scroll', () => {
-        if (landing.scrollHeight - window.scrollY <= 74) {
+        if (landing.scrollHeight - window.scrollY <= 73) {
             stickyNav.style.display = "flex";
         } else {
             stickyNav.style.display = 'none';
