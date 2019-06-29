b0e1a9581b1890249c416bb04bd961613c05d173
William M <wmooney1984@gmail.com>
2019-06-28 23:24:42 -0400

    Header.js bug fix

--- components/header/header.js
+++ components/header/header.js
@@ -1,3 +1,10 @@
+const iconsArray = ['<i class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i>', 
+                    '<i class="fab fa-facebook-f fa-2x" aria-hidden="true"></i>', 
+                    '<i class="fab fa-twitter fa-2x" aria-hidden="true"></i>', 
+                    '<i class="fab fa-instagram fa-2x" aria-hidden="true"></i>', 
+                    '<i class="fas fa-envelope fa-2x" aria-hidden="true"></i>'];
+
+
 let headerIcon = document.querySelector('.icons');
 
 iconsArray.forEach((elem)=> {
