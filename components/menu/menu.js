cd7efb329d904b95474c9075451b12b77ff50a2d
Dave Kane <davelkane@gmail.com>
2019-06-29 03:19:38 -0500

    Modal animation complete

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,13 +1,39 @@
 
 const menuToggle = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  // if (menu.hasAttribute('menu--open')) {
+
+  //   TweenMax.to('.menu', 0.4, { x: 0 });
+
+  //   menu.classList.toggle('menu--open');
+
+  // } else {
+
   menu.classList.toggle('menu--open');
+
+  if (document.querySelector('title').innerText.includes('Home')) {
+    mainContainer.classList.toggle('background-blur');
+  } else if (document.querySelector('title').innerText.includes('Features')) {
+    featuresContainer.classList.toggle('background-blur');
+  } else if (document.querySelector('title').innerText.includes('Team')) {
+    teamContainer.classList.toggle('background-blur');
+  } else if (document.querySelector('title').innerText.includes('Sign Up')) {
+    signUpContainer.classList.toggle('background-blur');
+  }
+
+  TweenMax.from('.menu', 0.4, { x: 350 });
 }
+// }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+
+const mainContainer = document.querySelector('.main-container');
+const featuresContainer = document.querySelector('.features-container');
+const teamContainer = document.querySelector('.team-container');
+const signUpContainer = document.querySelector('#sign-up-page');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', menuToggle);
 
