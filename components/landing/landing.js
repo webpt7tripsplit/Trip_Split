b79619931ef2eac596491887828bb91e5200abe5
Dave Kane <davelkane@gmail.com>
2019-06-29 19:00:53 -0500

    Menu and landing page animations complete

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -14,6 +14,8 @@ if (document.querySelector('title').innerText.includes('Home')) {
     window.addEventListener('scroll', () => {
         if (landing.scrollHeight - window.scrollY <= 73) {
             stickyNav.style.display = "flex";
+            TweenMax.to ('#menu-bar', 2, {opacity: 1});
+
         } else {
             stickyNav.style.display = 'none';
         }
