412742f6eb8d919e33c842b7504b9a2df8038a33
Dave Kane <davelkane@gmail.com>
2019-06-30 20:37:38 -0500

    Clean Up

--- components/main-container/main-container.js
+++ components/main-container/main-container.js
@@ -2,4 +2,4 @@ const mainBackground = document.querySelector('.main-container');
 const mainCompStyles = getComputedStyle(mainBackground);
 const bgImage = mainCompStyles.backgroundImage;
 
-TweenMax.from(bgImage, 2, {opacity: 0, delay: 1});
+// TweenMax.from(bgImage, 2, {opacity: 0, delay: 1});
